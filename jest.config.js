ccbee4d13237ececa71b83b8032486ab8e8971fd
Pedro Ribeiro <pri@atobi.io>
2024-04-16 17:56:12 +0100

    finish challenge

--- jest.config.js
+++ jest.config.js
@@ -11,9 +11,6 @@ module.exports = {
     "^@validators/(.*)$": "<rootDir>/src/helpers/$1",
     "^@enums/(.*)$": "<rootDir>/src/enums/$1",
     "^@schemas/(.*)$": "<rootDir>/src/schemas/$1",
-    "^src/helpers/(.*)$": "<rootDir>/src/helpers/$1",
     "^src/functions/(.*)$": "<rootDir>/src/functions/$1",
-
-    // Add more mappings as needed
   },
 };
